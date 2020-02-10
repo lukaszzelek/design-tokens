6a3a2825d784a359b18ea1ef4560cb261c8901ec
Krzysztof Pyrzyk <krzysztof.pyrzyk@airhelp.com>
2020-02-10 11:39:56 +0100

    Fixed line-heights for Typography

--- src/components/Typography/Paragraph/Paragraph.stories.js
+++ src/components/Typography/Paragraph/Paragraph.stories.js
@@ -4,5 +4,9 @@ import Paragraph from './Paragraph';
 export default { title: 'Typography' };
 
 export const Paragraphs = () => (
-  <Paragraph>This is a paragraph!</Paragraph>
+  <>
+    <Paragraph>This is a paragraph!</Paragraph>
+    <Paragraph>This is a<br />multiline paragraph!<br />This it it's third line!</Paragraph>
+  </>
+
 );
