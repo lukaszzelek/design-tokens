b27cacf8e68d78b25daf8b3e7a5829c32d5a70c6
Krzysztof Pyrzyk <krzysztof.pyrzyk@airhelp.com>
2020-02-10 09:44:50 +0100

    Update sizes for Header

--- src/components/Typography/Header/Header.js
+++ src/components/Typography/Header/Header.js
@@ -3,8 +3,21 @@ import PropTypes from 'prop-types';
 
 import "./Header.scss";
 
+const mapSizeToTag = size => {
+  const sizes = {
+    "xs": 6,
+    "s": 5,
+    "m": 4,
+    "l": 3,
+    "xl": 2,
+    "xxl": 1
+  };
+
+  return size in sizes ? sizes[size] : 1;
+}
+
 const Header = ({ size, className, children }) => {
-  const Tag = `h${size}`;
+  const Tag = `h${mapSizeToTag(size)}`;
 
   return (
     <Tag className={className}>
--- src/components/Typography/Header/Header.stories.js
+++ src/components/Typography/Header/Header.stories.js
@@ -5,11 +5,11 @@ export default { title: 'Typography' };
 
 export const Headers = () => (
   <>
-    <Header size="1">This is a H1</Header>
-    <Header size="2">This is a H2</Header>
-    <Header size="3">This is a H3</Header>
-    <Header size="4">This is a H4</Header>
-    <Header size="5">This is a H5</Header>
-    <Header size="6">This is a H6</Header>
+    <Header size="xxl">This is a H1</Header>
+    <Header size="xl">This is a H2</Header>
+    <Header size="l">This is a H3</Header>
+    <Header size="m">This is a H4</Header>
+    <Header size="s">This is a H5</Header>
+    <Header size="xs">This is a H6</Header>
   </>
 );
